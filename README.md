6f2cbca481da066981f6d8ac063c1151e589f508
Antonio Ribeiro <antonio@area17.com>
2022-07-18 13:02:50 +0200

    Add path ignores

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HTTP Basic Auth
 
+## TODO: Add to install
+
+```
+php artisan vendor:publish --provider="A17\HttpBasicAuth\ServiceProvider"
+```
--- config/http-basic-auth.php
+++ config/http-basic-auth.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
-    /**
-     * Enable/disable the package
-     */
     'enabled' => env('HTTP_BASIC_AUTH_ENABLED', true),
 
-    'username' => env('HTTP_BASIC_USERNAME', 'username'),
+    'username' => env('HTTP_BASIC_USERNAME', 'usern@me'),
 
     'password' => env('HTTP_BASIC_PASSWORD', 'passw0rt'),
+
+    'routes' => [
+        'ignore' => [
+            'paths' => ['/admin/*', '/api/v1/*'],
+        ],
+    ],
 ];
